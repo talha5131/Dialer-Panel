f1c7d2a9d09aa242504ea6ba6cfc2bc778b85811
talha5131 <talhamalik4147@gmail.com>
2020-08-16 17:24:40 -0700

    Aug 16,2020 5:24

---
+++ crud.php
@@ -0,0 +1,37 @@
+<?php
+    // include("index.php");
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "clients data";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    if ($conn) {
+        echo "Connection OK";
+    }
+    else{
+        die("Connection Failed Because Of ".mysqli_connect_error());
+    }
+
+    $firstName = $_GET['first_name'];
+    $lastName = $_GET['last_name'];
+    $company = $_GET['company'];
+    $rank = $_GET['rank'];
+    $email = $_GET['email'];
+    $phone = $_GET['phone'];
+    $address = $_GET['address'];
+    $website = $_GET['website'];
+    $city = $_GET['city'];
+    $state = $_GET['state'];
+    $postCode = $_GET['postCode'];
+    $country = $_GET['country'];
+    $companyHistory = $_GET['companyHistory'];
+    $requirements = $_GET['requirements'];
+
+    $insertQuery = "INSERT INTO clients VALUES ('$firstName','$lastName','$company','$rank','$email','$phone','$address',
+    '$website','$city','$state','$postCode','$country','$companyHistory','$requirements')";
+
+    // echo $firstName;
+    mysqli_query($conn, $insertQuery);
+?>
---
+++ index2.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Colorlib Templates">
+    <meta name="author" content="Colorlib">
+    <meta name="keywords" content="Colorlib Templates">
+
+    <!-- Title Page-->
+    <title>Registration Form</title>
+
+    <!-- Icons font CSS-->
+    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <!-- Font special for pages-->
+    <link
+        href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Vendor CSS-->
+    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="css/main.css" rel="stylesheet" media="all">
+
+    <!-- Richtext Plugin For Textarea -->
+    <link rel="stylesheet" href="css/richtext.min.css">
+</head>
+
+<body>
+    <p id="demo"></p>
+    <?php
+        echo "OK Yes";
+    ?> 
+</body>
+<script src="vendor/jquery/jquery.min.js"></script>
+
+<!-- <script type="text/javascript">
+    var clientsDataUpdate = JSON.parse(localStorage['clientsData']);
+    $('#demo').text(clientsDataUpdate[0].firstName);
+</script> -->
+<!-- This templates was made by Colorlib (https://colorlib.com) -->
+
+</html>
+<!-- end document-->
